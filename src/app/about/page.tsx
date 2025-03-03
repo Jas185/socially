4d9f3ecc8fd82ccd63266483b0f216024ad78fd0
Jas185 <semmouissi@gmail.com>
2025-03-03 23:17:10 +0100

    page de signin fonctionnel

---
+++ src/app/about/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function AboutPage() {
+    console.log("Hello from about page"); 
+  return (
+    <div>Hello from about page</div>
+  )
+}
+
+export default AboutPage
