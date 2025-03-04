26c48499b532a9fa82bd5b7bc512e85f7150b177
Jas185 <semmouissi@gmail.com>
2025-03-04 16:21:14 +0100

    ajout du profil,post et commentaire

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,30 @@
- import { Button } from "@/components/ui/button";
-import ModeToggle from "@/components/ModeToggle";
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
-import Image from "next/image";
+import { getPosts } from "@/action/post.action";
+import { getDbUserId } from "@/action/user.action";
+import CreatePost from "@/components/CreatePost";
+import PostCard from "@/components/PostCard";
+import WhoToFollow from "@/components/WhoToFollow";
+import { currentUser } from "@clerk/nextjs/server";
 
-export default function Home() {
+export default async function Home() {
+  const user = await currentUser();
+  const posts = await getPosts();
+  const dbUserId = await getDbUserId();
+
   return (
-    <div className="m-4">
-<SignedOut>
-  <SignInButton mode='modal'>
-    <Button className="bg-red-500">
-      Sign in
-    </Button>
-  </SignInButton>
-</SignedOut>
+    <div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
+      <div className="lg:col-span-6">
+        {user ? <CreatePost /> : null}
 
-  <SignedIn>
-    <UserButton />
-  </SignedIn>
+        <div className="space-y-6">
+          {posts.map((post) => (
+            <PostCard key={post.id} post={post} dbUserId={dbUserId} />
+          ))}
+        </div>
+      </div>
 
-  <ModeToggle />
-  
-  <Button variant={"secondary"}>
-    Click me
-  </Button>
+      <div className="hidden lg:block lg:col-span-4 sticky top-20">
+        <WhoToFollow />
+      </div>
     </div>
   );
 }
---
+++ src/lib/uploadthing.ts
@@ -0,0 +1,6 @@
+
+import { OurFileRouter } from "@/app/uploadthing/core";
+import { generateUploadButton, generateUploadDropzone } from "@uploadthing/react";
+
+export const UploadButton = generateUploadButton<OurFileRouter>();
+export const UploadDropzone = generateUploadDropzone<OurFileRouter>();
