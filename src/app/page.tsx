bdf7a77218e0697b8f44a3c923bc84c6f4f648b2
Jas185 <semmouissi@gmail.com>
2025-03-04 02:24:34 +0100

    ajout de prisma

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,28 @@
- import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+ import { Button } from "@/components/ui/button";
+import ModeToggle from "@/components/ModeToggle";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
+    <div className="m-4">
 <SignedOut>
   <SignInButton mode='modal'>
-    <button className="bg-red-500">
-
-    </button>
+    <Button className="bg-red-500">
+      Sign in
+    </Button>
   </SignInButton>
 </SignedOut>
 
   <SignedIn>
     <UserButton />
   </SignedIn>
+
+  <ModeToggle />
+  
+  <Button variant={"secondary"}>
+    Click me
+  </Button>
     </div>
   );
 }
